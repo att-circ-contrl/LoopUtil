3fe8a422bd62aad13effc88f6dbf6e92983aaa07
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2023-04-24 16:28:49 -0500

    Added fillNaN wrapper for use with Field Trip datasets.

---
+++ libraries/lib-nloop-ft/nlFT_fillNaN.m
@@ -0,0 +1,35 @@
+function newftdata = nlFT_fillNaN( oldftdata )
+
+% function newftdata = nlFT_fillNaN( oldftdata )
+%
+% This calls nlProc_fillNaN() to interpolate NaN segments within all trials
+% of the supplied Field Trip dataset.
+%
+% "oldftdata" is a ft_dataset_raw dataset to modify.
+%
+% "newftdata" is a copy of "oldftdata" with NaN segments interpolated.
+
+
+trialcount = length(oldftdata.trial);
+chancount = length(oldftdata.label);
+
+newftdata = oldftdata;
+
+for tidx = 1:trialcount
+  thistrial = newftdata.trial{tidx};
+
+  for cidx = 1:chancount
+    thiswave = thistrial(cidx,:);
+    thistrial(cidx,:) = nlProc_fillNaN(thiswave);
+  end
+
+  newftdata.trial{tidx} = thistrial;
+end
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
