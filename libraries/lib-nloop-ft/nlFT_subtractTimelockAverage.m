c942c7c292f380d9401236919005544026d0693e
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2023-05-05 16:51:14 -0500

    Tweaked background subtraction to average background but apply to background and non-background.

--- libraries/lib-nloop-ft/nlFT_subtractTimelockAverage.m
+++ libraries/lib-nloop-ft/nlFT_subtractTimelockAverage.m
@@ -7,6 +7,9 @@ function newtimelock = ...
 % This subtracts the average response across channels from each channel in
 % oldtimelock.avg. The idea is to make local changes in response more visible.
 %
+% The average is computed using a subset of the channels, but subtracted from
+% all channels.
+%
 % "oldtimelock" is a dataset returned by ft_timelockanalysis.
 % "avgchans" is a cell array containing channel labels to use for building
 %   the average, or {} to use all channels.
@@ -32,6 +35,8 @@ end
 
 if any(chanmask)
 
+  % Build the average using only the indicated channels.
+
   avgtotal = zeros(size(newtimelock.time));
 
   for cidx = 1:length(chanmask)
@@ -42,11 +47,12 @@ if any(chanmask)
 
   avgtotal = avgtotal / sum(chanmask);
 
+
+  % Subtract the average from all channels.
+
   for cidx = 1:length(chanmask)
-    if chanmask(cidx)
-      thisslice = newtimelock.avg(cidx,:);
-      newtimelock.avg(cidx,:) = thisslice - avgtotal;
-    end
+    thisslice = newtimelock.avg(cidx,:);
+    newtimelock.avg(cidx,:) = thisslice - avgtotal;
   end
 
 end
