e1afe8b971ea9d57d14e1dc9128bb10fa3a7cfbc
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2023-08-21 14:34:31 -0500

    Added outlier-detection function based on distance from the median.

---
+++ libraries/lib-nloop-proc/nlProc_getOutliers.m
@@ -0,0 +1,42 @@
+function outliervec = nlProc_getOutliers( ...
+  dataseries, lowperc, highperc, lowmult, highmult )
+
+% function outliervec = nlProc_getOutliers( ...
+%   dataseries, lowperc, highperc, lowmult, highmult )
+%
+% This function flags outliers in a data series based on their distance from
+% the median.
+%
+% "dataseries" is a vector containing samples to process.
+% "lowperc" is the percentile from which the lower threshold is derived
+%   (e.g. "25" for the lower quartile).
+% "highperc" is the percentile from which the upper threshold is derived
+%   (e.g. "75" for the upper quartile).
+% "lowmult" is a multiplier for generating the lower outlier threshold. The
+%   distance from the median to the lower percentile value is multiplied by
+%   this amount.
+% "highmult" is a multiplier for generating the upper outlier threshold. The
+%   distance from the median to the upper percentile value is multiplied by
+%   this amount.
+%
+% "outliervec" is a boolean vector of the same size as "dataseries" that's
+%   true for data samples past the outlier thresholds and false otherwise.
+
+
+percvals = prctile( dataseries, [ lowperc, 50, highperc ] );
+lowval = percvals(1);
+midval = percvals(2);
+highval = percvals(3);
+
+threshlow = midval + (lowval - midval) * lowmult;
+threshhigh = midval + (highval - midval) * highmult;
+
+outliervec = (dataseries <= threshlow) | (dataseries >= threshhigh);
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
