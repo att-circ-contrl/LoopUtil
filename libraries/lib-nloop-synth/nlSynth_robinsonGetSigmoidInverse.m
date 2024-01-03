fad4afbb213a76c0f17df3680ad0c1ad20d5470b
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-01-03 09:47:41 -0600

    Added sigmoid inverse and derivative functions.

---
+++ libraries/lib-nloop-synth/nlSynth_robinsonGetSigmoidInverse.m
@@ -0,0 +1,32 @@
+function potential = nlSynth_robinsonGetSigmoidInverse( ...
+  firingrate, maxrate, threshlevel, threshdeviation )
+
+% function potential = nlSynth_robinsonGetSigmoidInverse( ...
+%   firingrate, maxrate, threshlevel, threshdeviation )
+%
+% This converts a firing rate (Q in Robinson's model) back to a cell-body
+% potential (V in Robinson's model), performing the inverse of
+% nlSynth_robinsonGetSigmoid().
+%
+% "firingrate" is the firing rate (Q, or S(V)). This may be a vector or
+%   matrix, to convert several firing rates into potentials.
+% "maxrate" is the maximum firing rate (Q_max).
+% "threshlevel" is the average neuron threshold (theta).
+% "threshdeviation" is the standard deviation of the average neuron threshold
+%   (sigma - not sigma prime!).
+%
+% "potential" is the cell body potential (V).
+
+
+sigmaprime = threshdeviation * sqrt(3) / pi;
+
+scratch = maxrate ./ firingrate;
+potential = threshlevel - sigmaprime * log(scratch - 1);
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
