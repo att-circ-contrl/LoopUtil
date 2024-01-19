a673a467b07c2f655fcc684d5f37a06bb9ca4ffb
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-01-19 13:49:04 -0600

    Moved peak detection helpers to this library.

---
+++ libraries/lib-nloop-proc/nlProc_findPeakLargest.m
@@ -0,0 +1,36 @@
+function peakidx = nlProc_findPeakLargest( dataseries )
+
+% function peakidx = nlProc_findPeakLargest( dataseries )
+%
+% This finds the sample index of the largest peak in the supplied data
+% series.
+%
+% This finds the peak with the largest magnitude (ignoring sign and complex
+% phase angle).
+%
+% "dataseries" is a vector to search.
+%
+% "peakidx" is the sample number of the sample in "dataseries" with the
+%   largest magnitude.
+
+
+% Tolerate the "empty data" case.
+peakidx = NaN;
+
+if ~isempty(dataseries)
+  magvals = abs(dataseries);
+  [ magvals, sortidx ] = sort(magvals);
+
+  % Change ascending order to descending order.
+  sortidx = flip(sortidx);
+
+  peakidx = sortidx(1);
+end
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
---
+++ libraries/lib-nloop-proc/nlProc_findPeakNearest.m
@@ -0,0 +1,67 @@
+function peakidx = nlProc_findPeakNearest( datavals, timevals, timetarget )
+
+% function peakidx = nlProc_findPeakNearest( datavals, timevals, timetarget )
+%
+% This finds the sample index of the peak in the supplied data series that
+% is closest to the specified time series location.
+%
+% Peaks are local maxima of magnitude (ignoring sign and complex phase angle).
+%
+% "datavals" is a vector with data series values.
+% "timevals" is a vector with time series values.
+% "timetarget" is the time value to search near. This doesn't have to be
+%   present in "timevals" (distance to it is minimized).
+%
+% "peakidx" is the sample number of the sample in "datavals" which is the
+%   magnitude local maximum closest to the specified starting time.
+
+
+%
+% First pass: Identify peaks.
+
+sampcount = length(datavals);
+
+% This shortens the series by one sample.
+% Tolerate nonuniform time sampling.
+diffvals = diff(datavals) ./ diff(timevals);
+
+% Look for zero-crossings in the derivative to find extrema.
+% This shortens the series by a second sample.
+diffpositive = diffvals >= 0;
+diffcount = length(diffpositive);
+peakmask = ( diffpositive(1:diffcount-1) ~= diffpositive(2:diffcount) );
+
+% Truncate the input series to match the new length.
+datavals = datavals(2:diffcount+1);
+timevals = timevals(2:diffcount+1);
+
+% Get a matching index series.
+idxvals = 2:(diffcount+1);
+
+% Convert all of these into sparse series.
+datavals = datavals(peakmask);
+timevals = timevals(peakmask);
+idxvals = idxvals(peakmask);
+
+
+%
+% Second pass: Find the peak closest to the starting time.
+
+% Tolerate the "we found no peaks" case. A ramp will do that.
+peakidx = NaN;
+
+if ~isempty(datavals)
+  distancevals = abs(timevals - timetarget);
+  [ distancevals, sortidx ] = sort(distancevals);
+  bestidx = sortidx(1);
+
+  peakidx = idxvals(bestidx);
+end
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
