6304f221c38c89d0ab4b47fdf958c9c1a7b9f55b
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-09-10 12:12:48 -0500

    Added stride option for sliding window polynomial fit and artifact removal.

--- libraries/lib-nloop-artifact/nlArt_calcSlidingWindowPolyFit.m
+++ libraries/lib-nloop-artifact/nlArt_calcSlidingWindowPolyFit.m
@@ -1,8 +1,8 @@
 function fitwave = ...
-  nlArt_calcSlidingWindowPolyFit( srcwave, winsamps, polyorder )
+  nlArt_calcSlidingWindowPolyFit( srcwave, winsamps, polyorder, stridesamps )
 
 % function fitwave = ...
-%   nlArt_calcSlidingWindowPolyFit( srcwave, winsamps, polyorder )
+%   nlArt_calcSlidingWindowPolyFit( srcwave, winsamps, polyorder, stridesamps )
 %
 % This performs a polynomial fit within a sliding window, returning a signal
 % reconstructed by evaluating the polynomial at the window's midpoint.
@@ -10,12 +10,15 @@ function fitwave = ...
 % This functions as a low-pass filter with a corner wavelength comparable
 % to the window size.
 %
-% NOTE - Because this is evaluated for every sample, it takes a while for
-% large window sizes and high sampling rates.
+% NOTE - Evaluating the window for every sample will take a while for large
+% window sizes and high sampling rates. Decimate using "stride" to speed
+% this up.
 %
 % "srcwave" is the signal to curve fit.
 % "winsamps" is the number of samples in the sliding window.
 % "polyorder" is the order of the polynomial to fit.
+% "stridesamps" is the number of samples by which to slide the window on
+%   each step.
 %
 % "fitwave" is the smoothed signal produced by the sliding window curve fit.
 
@@ -31,7 +34,8 @@ winrad = max(winrad, 1);
 % Precompute in-window times.
 wintimes = (-winrad):winrad;
 
-% Walk through the input, curve fitting at each sample.
+
+% Walk through the input, curve fitting at each stride point.
 % FIXME - This is a brute force implementation. There's probably a faster way.
 
 % Make a padded input wave, for convenience.
@@ -39,7 +43,7 @@ sampcount = length(srcwave);
 padwave = NaN([ 1 (sampcount + winrad + winrad) ]);
 padwave( (1 + winrad):(sampcount+winrad) ) = srcwave(:);
 
-for sidx = 1:sampcount
+for sidx = 1:stridesamps:sampcount
   % (sidx - winrad) + winrad to (sidx + winrad) + winrad.
   thiswave = padwave( sidx:(sidx + winrad + winrad) );
 
@@ -56,6 +60,10 @@ for sidx = 1:sampcount
     fitwave(sidx) = polyval(coeffs, 0);
   end
 end
+
+
+% Interpolate between our stride points. This also handles endpoints.
+fitwave = nlProc_interpolateSeries(1:sampcount, fitwave, 1:sampcount );
 
 
 % Done.
--- libraries/lib-nloop-ft/nlFT_removeSlidingWindowPolyFit.m
+++ libraries/lib-nloop-ft/nlFT_removeSlidingWindowPolyFit.m
@@ -1,20 +1,24 @@
-function newftdata = ...
-  nlFT_removeSlidingWindowPolyfit( oldftdata, winsecs, polyorder )
+function newftdata = nlFT_removeSlidingWindowPolyfit( ...
+  oldftdata, winsecs, polyorder, stridefrac )
 
-% function newftdata = ...
-%   nlFT_removeSlidingWindowPolyfit( oldftdata, winsecs, polyorder )
+% function newftdata = nlFT_removeSlidingWindowPolyfit( ...
+%   oldftdata, winsecs, polyorder, stridefrac )
 %
 % This is a wrapper for nlArt_calcSlidingWindowPolyFit().
 %
 % This performs a sliding window polynomial fit to remove low-frequency
 % components, which are presumed to be artifact excursions.
 %
-% NOTE - Because this is evaluated for every sample, it takes a while for
-% large window sizes and high sampling rates.
+% NOTE - Evaluating the window for every sample will take a while for large
+% window sizes and high sampling rates. Decimate using "stride" to speed
+% this up.
 %
 % "oldftdata" is a ft_datatype_raw structure with trial data to process.
 % "winsecs" is the sliding window duration in seconds.
 % "polyorder" is the polynomial order for curve fitting.
+% "stridefrac" is the fraction of the window width by which to slide the
+%   window on each step. Use 0 to step by one sample. This should be less
+%   than 1/polyorder to capture curve fit details (ideally much less).
 %
 % "newftdata" is a copy of "oldftdata" with the curve fits subtracted.
 
@@ -34,6 +38,10 @@ if trialcount > 0
 end
 
 winsamps = round(winsecs * samprate);
+winsamps = max(1, winsamps);
+
+stridesamps = round(winsamps * stridefrac);
+stridesamps = max(1, stridesamps);
 
 
 % Iterate trials and channels, subtracting curve fits.
@@ -45,7 +53,8 @@ for tidx = 1:trialcount
   for cidx = 1:chancount
     thiswave = thistrial(cidx,:);
 
-    thisfit = nlArt_calcSlidingWindowPolyFit( thiswave, winsamps, polyorder );
+    thisfit = nlArt_calcSlidingWindowPolyFit( ...
+      thiswave, winsamps, polyorder, stridesamps );
 
     thistrial(cidx,:) = thiswave - thisfit;
   end
